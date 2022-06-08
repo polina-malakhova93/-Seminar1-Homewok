afb7387d900bdb99ea60df929711ae3728ac4f7e
polina993 <>
2022-06-08 16:09:34 +0300

    add task8

---
+++ 8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+double number2 = 1;
+
+while (number2 <= number)
+{
+    if (number2%2==0)
+    {
+        Console.Write(number2 + " ");
+    }
+    number2++;
+}
