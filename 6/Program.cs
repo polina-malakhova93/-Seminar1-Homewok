dbd6d1eec38e1f0ae235762dee7be967d87e6731
polina993 <>
2022-06-08 15:26:13 +0300

    add task6

---
+++ 6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    Console.WriteLine( " число является четным ");
+}
+else 
+{
+    Console.WriteLine(" число не является четным ");
+}
